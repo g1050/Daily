69f8b5c50c3ce979e22a18ccdce4135473dca20f
sky <1005035266@qq.com>
2019-09-29 13:59:40 +0800

    支持小数

--- DataStructure/stackandqueue/exp.hpp
+++ DataStructure/stackandqueue/exp.hpp
@@ -5,6 +5,8 @@
 #include "stack.hpp"
 #include <vector>
 #include <ctype.h>
+#include <algorithm>
+#include <cmath>
 using namespace std;
 
 class Exp
@@ -17,62 +19,71 @@ public:
     bool ifLegal();
     void toPostfix();
     int getPriority(char c);
-    int getValue();
+    double getValue();
 private:
     string s;
     vector <char> v;
 };
 
-int Exp::getValue()
+double Exp::getValue()
 {
     int size = v.size();
-    stack <int> st;
+    stack <double> st;
     for(int i = 0;i<size;i++){
         
         if(isalnum(v[i])){
-            int res = v[i]-48;
+            int cnt = -1;//标记是否有小数点后面几位
+            double res = v[i]-48;
             while(i+1 < size && isalnum(v[i+1])){//处理两位数及两位数以上 后面必接一个#
                 res *= 10;
                 res += (v[++i]-48);
             }
-            /* cout << res << endl; */
+            if(i+1 < size && v[i+1] == '.'){
+                i++;//跳过小数点开始扫描后面的数字
+                while(i+1 < size && isalnum(v[i+1])){
+                    res += (v[++i]-48)*pow(10,cnt--);
+                }
+            }
+
+            /* cout << "element = " << res << endl; */
             st.push(res);
             i++;//跳过#
         }else{
             if(v[i] == '+'){
-                int top1 = st.top();
+                double top1 = st.top();
                 st.pop();
-                int top2 = st.top();
+                double top2 = st.top();
                 st.pop();
-                int res = top1 + top2;
+                double res = top1 + top2;
                 /* cout << res << endl; */
                 st.push(res);
             }else if(v[i] == '-'){
-                int top1 = st.top();
+                double top1 = st.top();
                 st.pop();
-                int top2 = st.top();
+                double top2 = st.top();
                 st.pop();
-                int res = top2 - top1;
+                double res = top2 - top1;
                 st.push(res);
                 /* cout << res << endl; */
             }
             
             else if(v[i] == '*'){
-                int top1 = st.top();
+                double top1 = st.top();
                 st.pop();
-                int top2 = st.top();
+                double top2 = st.top();
                 st.pop();
-                int res = top1 * top2;
+                double res = top1 * top2;
                 st.push(res);
                 /* cout << res << endl; */
             }else if(v[i] == '/'){
-                int top1 = st.top();
+                double top1 = st.top();
                 st.pop();
-                int top2 = st.top();
+                double top2 = st.top();
                 st.pop();
-                int res = top2 /  top1;
+                /* cout << top1 << " " << top2 << endl; */
+                double res = top2 /  top1;
                 st.push(res);
-                /* cout << res << endl; */
+              /* cout << res << endl; */
             }
         }
     }
@@ -136,10 +147,10 @@ void Exp::toPostfix()
     
        if(isalnum(s[i])){
            v.push_back(s[i]);
-           while(i+1 < size && isalnum(s[i+1])){
+           while(i+1 < size && ( isalnum(s[i+1]) || s[i+1] == '.')){
                v.push_back(s[++i]);
            }
-           v.push_back('#');//＃隔开 12+3 后缀表达式为12#3+
+           v.push_back('#');//＃隔开 12+3 后缀表达式为12#3+ 12.3+3  12.3#3+
        }
        else if(st.empty()){
            st.push(s[i]);
@@ -170,6 +181,7 @@ void Exp::toPostfix()
    }
 
    for(int i = 0;i<(int)v.size();i++) cout << v[i];
+   cout << "\n";
    return ;
 }
 #endif
