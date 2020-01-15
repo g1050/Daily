e305204a2cdda01719a92a7f53a3a4e1c4536c8e
sky <1005035266@qq.com>
2020-01-15 20:51:03 +0800

    mystring begin

--- JAVA/src/Fz106.java
+++ JAVA/src/Fz106.java
@@ -1,4 +1,94 @@
-package PACKAGE_NAME;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import zdsd.Circle;
+
+import javax.swing.text.html.ImageView;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+abstract class AbstractFile {
+    protected String name;
+
+    public void printName() {
+        System.out.println(name);
+    }
+
+    public abstract boolean addChild(AbstractFile file);
+
+    public abstract boolean removeChild(AbstractFile file);
+
+    public abstract List<AbstractFile> getChildren();
+}
+
+class File extends AbstractFile {
+    public File(String name) {
+        this.name = name;
+    }
+
+    public boolean addChild(AbstractFile file) {
+        return false;
+    }
+
+    public boolean removeChild(AbstractFile file) {
+        return false;
+    }
+
+    public List<AbstractFile> getChildren() {
+        return null;
+    }
+}
+
+class Folder extends AbstractFile {
+    private List<AbstractFile> childList;
+
+    public Folder(String name) {
+        this.name = name;
+        this.childList = new ArrayList<AbstractFile>();
+    }
+
+    public boolean addChild(AbstractFile file) {
+        return childList.add(file);
+    }
+
+    public boolean removeChild(AbstractFile file) {
+        return childList.remove(file);
+    }
+
+    public List <AbstractFile> getChildren() { return
+                childList;
+    }
+}
 
 public class Fz106 {
+    public static void main(String[] args) {
+        // 构造一个树形的文件/目录结构
+        AbstractFile rootFolder = new Folder("root");
+        AbstractFile compositeFolder = new Folder("composite");
+        AbstractFile windowsFolder = new Folder("windows");
+        AbstractFile file = new File("TestComposite.java");
+        compositeFolder.addChild(file);
+        rootFolder.addChild(compositeFolder);
+        rootFolder.addChild(windowsFolder);
+
+        // 打印目录文件树
+        printTree(rootFolder);
+    }
+
+    private static void printTree(AbstractFile ifile) {
+        ifile.printName();
+        List<AbstractFile> children = ifile.getChildren();
+        if (children == null)
+            return;
+        for (AbstractFile file : children) {
+
+            System.out.print(file);
+        }
+    }
+    int a[] = new int [10];
+    int len = a.length;
+
+    Scene scene  = new Scene(new Pane());
+    ;;
+
 }
--- JAVA/src/Six/Fz134.java
+++ JAVA/src/Six/Fz134.java
@@ -1,4 +1,88 @@
 package Six;
 
-public class Fz134 {
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+public class Fz134 extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            VBox box = new VBox();
+            FlowPane flowPane = new FlowPane();
+            GridPane gridPane = new GridPane();
+            box.getChildren().add(flowPane);
+            box.getChildren().add(gridPane);
+            TextField Number1 = new TextField();
+            Number1.setPrefSize(50, 20);
+            TextField Number2 = new TextField();
+            Number2.setPrefSize(50, 20);
+            TextField Result = new TextField();
+            Result.setPrefSize(100, 20);
+            Result.setEditable(false);
+
+            flowPane.getChildren().addAll(new Label("Number1:"), Number1, new Label("Number2:"), Number2,
+                    new Label("Result:"), Result);
+
+            Button Add = new Button("Add");
+            // Create the handler class (listener class)
+            class AddHandler implements EventHandler<ActionEvent> {
+            @Override // Override the handle method
+            public void handle(ActionEvent e) {
+                Result.setText("" + (Double.parseDouble(Number1.getText()) + Double.parseDouble(Number2.getText())));
+            }
+			  }
+            // register the handler
+            Add.setOnAction(new AddHandler());
+
+            Button Subtract = new Button("Subtract");
+            // Create and register the handler (listener )
+            Subtract.setOnAction(new EventHandler<ActionEvent>() {
+                @Override // Override the handle method
+                public void handle(ActionEvent e) {
+                    Result.setText("" + (Double.parseDouble(Number1.getText()) - Double.parseDouble(Number2.getText())));
+                }
+            });
+
+            Button Multiply = new Button("Multiply");
+            Multiply.setOnAction(e-> {
+                Result.setText("" + (Double.parseDouble(Number1.getText()) * Double.parseDouble(Number2.getText())));
+            });
+
+            Button Divide = new Button("Divide");
+            Divide.setOnAction(e -> {
+                try {
+                    if(Double.parseDouble(Number2.getText())==0)
+                        throw new ArithmeticException("Divisor cannot be zero");
+                    Result.setText(
+                            "" + (Double.parseDouble(Number1.getText()) / Double.parseDouble(Number2.getText())));
+                } catch (ArithmeticException ex) {
+                    Result.setText("除数为零错误！");
+                }
+            });
+
+            gridPane.add(Add, 1, 0);
+            gridPane.add(Subtract, 2, 0);
+            gridPane.add(Multiply, 3, 0);
+            gridPane.add(Divide, 4, 0);
+            Scene scene = new Scene(box, 320, 400);
+            primaryStage.setTitle("Calculator"); // Set the stage title
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
